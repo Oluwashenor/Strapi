54ff1c2abe2676ffb00795f8d0b24bce69905fc8
Adeshina <adeshiname@gmail.com>
2023-01-10 13:48:05 +0100

    exercise-4

---
+++ src/api/food/routes/food.ts
@@ -0,0 +1,59 @@
+/**
+ * food router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default {
+    routes: [
+      { // Path defined with a URL parameter
+        method: 'GET',
+        path: '/food/suggest',
+        handler: 'food.suggestFood',
+        config:{
+                auth: false,
+                policies: [],
+                middlewares: [],
+        }
+      },
+      { // Path defined with a URL parameter
+        method: 'GET',
+        path: '/food/:id',
+        handler: 'food.get',
+        config:{
+                auth: false,
+                policies: [],
+                middlewares: [],
+        }
+      },
+
+      { 
+        method: 'GET',
+        path: '/food',
+        handler: 'food.getAll',
+        config:{
+                auth: false,
+                policies: [],
+                middlewares: [],
+        }
+      },
+
+      {
+        method:'POST',
+        path:'/food',
+        handler:'food.create',
+        config:{
+          auth:false
+        }
+      }
+      ,
+      {
+        method:'PUT',
+        path:'/food/:id',
+        handler:'food.update',
+        config:{
+          auth:false
+        }
+      }
+    ]
+  }
